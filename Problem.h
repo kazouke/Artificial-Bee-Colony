77430f7b8d55b7a798181d77eaa0b59bab5a5f0b
kazouke <kazouke@hotmail.fr>
2016-10-27 16:30:12 +0200

    Jolie organisation

---
+++ Problem.h
@@ -0,0 +1,26 @@
+#ifndef PROBLEM_H
+#define PROBLEM_H
+
+class Problem
+{
+public:
+	Problem();
+	~Problem();
+
+	friend ostream& operator<< (ostream& os, const Problem& pbm);
+	friend istream& operator>> (istream& is, Problem& pbm);
+
+	Problem& operator=  (const Problem& pbm);
+	bool operator== (const Problem& pbm) const;
+	bool operator!= (const Problem& pbm) const;
+
+	Direction direction () const; //Maximize or Minimize
+	int dimension() const;
+	double LowerLimit, UpperLimit;
+
+	private:
+
+		int _dimension;
+};
+
+#endif
