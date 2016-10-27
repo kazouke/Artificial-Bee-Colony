f255de0e858ed0390962ad0f3b7ba59566d05c56
Nilas-FR <benoit.sautour@laposte.net>
2016-10-27 15:49:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+sf#include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 /* Test pour Github - ICI C'EST LE MASTER*/
@@ -6,5 +6,6 @@
 int main(int argc, char** argv) {
 	std::cout<<"ok";
 	std::cout<<"jambon;
+	std::cout<<"parce qu'on aime ça !";
 	return 0;
 }
