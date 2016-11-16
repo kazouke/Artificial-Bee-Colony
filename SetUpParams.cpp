9411c97a327fdaf8e6fd380462b013bf7a58ec15
kazouke <kazouke@hotmail.fr>
2016-11-16 10:48:14 +0100

    Déclaration de SetUpParams

--- SetUpParams.cpp
+++ SetUpParams.cpp
@@ -24,3 +24,20 @@ const unsigned int SetUpParams::solution_size() const
 {
 	return _solution_size;
 }
+
+void SetUpParams::independent_runs(const unsigned int val)
+{
+	_independent_runs = val;
+}
+void SetUpParams::nb_evolution_steps(const unsigned int val)
+{
+	_nb_evolution_steps = val;
+}
+void SetUpParams::population_size(const unsigned int val)
+{
+	_population_size = val;
+}
+void SetUpParams::solution_size(const unsigned int val)
+{
+	_solution_size = val;
+}
