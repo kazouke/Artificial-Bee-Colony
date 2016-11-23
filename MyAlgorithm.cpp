92b9ad712b755f483fad9dd672a90fdb2ef3cfbb
Nilas-FR <benoit.sautour@laposte.net>
2016-11-23 20:08:18 +0100

    MaJ MyAlgorith
    Trier est OK
    Manque Init et Evolution

--- MyAlgorithm.cpp
+++ MyAlgorithm.cpp
@@ -1,5 +1,182 @@
 #include "MyAlgorithm.h"
 
+ABC::ABC(const Problem& pbm,const SetUpParams& setup):d_solutions(), d_fitnessValues(), d_setup(setup), d_upperCost(), d_lowerCost()
+{
+	d_solutions.resize(0);
+	d_fitnessValues.resize(0);
+	d_solutions.resize(d_setup.population_size());
+	d_fitnessValues.resize(d_setup.population_size());
+	for(int i=0;i<d_setup.population_size();++i)
+	{
+		d_solutions[i]=new Solution{pbm}; //le tableau solution
+        d_fitnessValues[i]=particle {.index=i,.fitness=d_solutions[i]->fitness()}; //index et fitness du tableau
+	}
+	trier();
+}
+
+/*
+ABC::ABC(const Problem& pbm,const SetUpParams& setup):d_solutions(), d_fitnessValues(), d_setup(setup), d_upperCost(), d_lowerCost()
+{
+	for(unsigned int i=0;i<d_setup.population_size();++i)
+    {
+        d_solutions.push_back(new Solution(pbm));
+        d_fitnessValues.push_back(particle {.index=i,
+                                            .fitness=d_solutions.back()->fitness()});
+    }
+}
+*/
+ABC::~ABC()
+{
+	for(int i=0;i<d_solutions.size();++i)
+	{
+		delete d_solutions[i];
+	}
+}
+
+
+std::ostream& operator<<(std::ostream& os, const ABC& myAlgo)
+{
+    for(int i{} ; i < myAlgo.d_solutions.size() ; ++i)
+        os << *(myAlgo.d_solutions[i]) << "\n";
+    for(int i{} ; i < myAlgo.d_fitnessValues.size() ; ++i)
+        os << myAlgo.d_fitnessValues[i].index << " : " << myAlgo.d_fitnessValues[i].fitness << "\n";
+    os << myAlgo.d_setup << "\n" << myAlgo.d_upperCost << " " << myAlgo.d_lowerCost;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, ABC& myAlgo)
+{
+    //is >> myAlgo.d_setup; Attendre SetUpParams
+    return is;
+}
+
+const SetUpParams& ABC::setup() const
+{
+	return d_setup;
+}
+
+void ABC::initialize() //renouvelle les solutions
+{
+	for(int i=0;i<d_solutions.size();++i)
+    {
+        delete d_solutions[i];
+        d_solutions[i] = new Solution{Problem{d_solutions[i]->pbm()}};
+		//Revoir
+    }
+}
+
+/*void ABC::evaluate()
+{
+	trier();
+    int m=d_solutions.size()/2;
+    for (int i = 0; i < m; ++i)
+    {
+        delete d_solutions[i+m];
+        d_solutions[i+m]=new Solution(*d_solutions[i]);
+    }
+    d_upperCost = d_fitnessValues.size()-1;
+    d_lowerCost = 0;
+	//Revoir
+}*/
+
+const vector<Solution*>& ABC::solutions() const
+{
+	return d_solutions;
+}
+
+int ABC::upper_cost() const
+{
+	return d_upperCost;
+}
+
+int ABC::lower_cost() const
+{
+	return d_lowerCost;
+}
+
+Solution& ABC::solution(const int index) const
+{
+	return *(d_solutions[index]);
+}
+
+vector<struct particle>&  ABC::fitness_values()
+{
+	return d_fitnessValues;
+}
+
+double ABC::fitness(const int index) const
+{
+	return d_fitnessValues[index].fitness;
+}
+
+double ABC::best_cost() const
+{
+	return d_solutions[d_lowerCost]->get_fitness();
+}
+
+double ABC::worst_cost() const
+{
+	return d_solutions[d_upperCost]->get_fitness();
+}
+
+/*Solution& ABC::best_solution() const
+{
+	
+}
+
+Solution& ABC::worst_solution() const
+{
+	
+}*/
+
+/*void ABC::evolution() //evolution(int iter), le nombre d'iteration est dans SetUpParams
+{
+	for (int i=0; i<d_setup.independent_runs(); ++i)
+	{
+		initialize();
+		double fitnessMin=d_solutions[0]->calFitness();
+	}
+}*/
+
+void ABC::trier()
+{
+	QuickSort(0,d_fitnessValues.size()-1);
+	d_lowerCost=0;
+	d_upperCost=d_fitnessValues.size()-1;
+}
+
+int ABC::partition(int gauche, int droite)
+{
+    double x=d_fitnessValues[gauche].fitness;
+    int i=gauche;
+    int j;
+
+    for(j=gauche+1; j<droite; j++)
+    {
+        if (d_fitnessValues[j].fitness<=x)
+        {
+            i=i+1;
+            std::swap(d_fitnessValues[i],d_fitnessValues[j]);
+        }
+
+    }
+
+    std::swap(d_fitnessValues[i],d_fitnessValues[gauche]);
+    return i;
+}
+
+void ABC::QuickSort(int gauche, int droite) //Tri Quicksort
+{
+	int r;
+    if(gauche<droite)
+    {
+        r=partition(gauche,droite);
+        QuickSort(gauche,r);  
+        QuickSort(gauche+1,droite);
+    }
+}
+
+/*
 int ABC::imin(int i) const
 {
     int r=i;
@@ -13,50 +190,4 @@ int ABC::imin(int i) const
     return r;
 }
 
-void ABC::trier()
-{
-    int indice = -1;
-    for(unsigned int i=0; i<d_solutions.size(); ++i)
-    {
-        indice=imin(i);
-        if(indice != i)
-        {
-            std::swap(d_solutions[i],d_solutions[indice]);
-            std::swap(d_fitnessValues[i].index,d_fitnessValues[indice].index);
-        }
-    }
-}
-
-ABC::ABC(const Problem& pbm,const SetUpParams& setup):d_solutions(), d_fitnessValues(), d_setup(setup), d_upperCost(), d_lowerCost()
-{
-	for(unsigned int i=0;i<d_setup.population_size();++i)
-    {
-        d_solutions.push_back(new Solution(pbm));
-        d_fitnessValues.push_back(particle {.index=i,
-                                            .fitness=d_solutions.back()->fitness()});
-    }
-	trier(); //trie les solutions par rapport à la fitness, croissant
-}
-
-ABC::~ABC()
-{
-	for(int i=0;i<d_solutions.size();++i)
-	{
-		delete d_solutions[i];
-	}
-}
-
-void ABC::launch()
-{
-	//Fonction utilisée pour les tests mais qui sera la boucle principale
-	
-	std::cout << "Tableau des fitness trie : " << std::endl;
-	trier();
-	for(int i=0; i < d_fitnessValues.size();i++)
-	{
-		std::cout << "Valeur " << i+1 <<" : fitness  : " << d_fitnessValues[i].fitness << " a la case " << d_fitnessValues[i].index << std::endl;
-	}
-	
-}
-
-
+*/
--- MyAlgorithm.h
+++ MyAlgorithm.h
@@ -10,6 +10,8 @@
 #include "Solution.h"
 
 using std::vector;
+using std::ostream;
+using std::istream;
 
 struct particle // index of a particle in the swarm and its fitness
 {
@@ -20,36 +22,43 @@ struct particle // index of a particle in the swarm and its fitness
 class ABC
 {
 	public:
-		ABC(const Problem& pbm,const SetUpParams& setup); 
-		~ABC();
+		ABC(const Problem& pbm,const SetUpParams& setup); 											//	OK
+		~ABC();																						//	OK
 
-		const SetUpParams& setup() const;															// retourne les paramètres
-	  	void initialize();	//Revoir																//
- 
+		friend 	ostream& operator<< (ostream& os, const ABC& myAlgo);								//	OK	A voir si utile
+		friend 	istream& operator>> (istream& is, ABC& myAlgo);										//	-	En attente de SetUpParams
+		//ABC& operator= (const ABC& myAlgo);														//	X	Inutile
+		const SetUpParams& setup() const;															//	OK	retourne les paramètres
+		void initialize();																			//	-	En attente de Solution
+
 		// creates a array with fitness of all solutions in ABC and its position in the ABC
-       void evaluate();	//Revoir																//
-	 
-	 	const vector<Solution*>& solutions() const;													//
-		int upper_cost() const;																		//
-		int lower_cost() const;																		//
-		Solution& solution(const int index) const;													//
-    	vector<struct particle>&  fitness_values();													//
-		double fitness(const int index) const;														//
-         
-		 
-		double best_cost() const;																	//
-		double worst_cost() const;																	//
+		void evaluate();																			//	X	
 
-		void trier();		// trie croissant des solutions par rapport à la fitness
-		int imin(int i) const;
+		const vector<Solution*>& solutions() const;													//	OK	
+		int upper_cost() const;																		//	OK	->Peu utile car les valeurs sont triées
+		int lower_cost() const;																		//	OK	->Peu utile car les valeurs sont triées
+		Solution& solution(const int index) const;													//	OK	
+		vector<struct particle>&  fitness_values();													//	OK	
+		double fitness(const int index) const;														//	OK	
+
+		double best_cost() const;																	//	OK	->Peu utile car les valeurs sont triées
+		double worst_cost() const;																	//	OK	->Peu utile car les valeurs sont triées
+//X		Solution& best_solution() const;															//	-	
+//X		Solution& worst_solution() const;															//	-	
+
+//X		void evolution();																			//	-	En attente des autres
+		void trier();																				//	OK	QuickSort
 		
-		void launch();	//lance l'algo ABC
 
 	private:
-		vector<Solution*> 		d_solutions;     													// individuals in population
-		vector<struct particle> d_fitnessValues;													//
-		const SetUpParams& 		d_setup;															//
-		int 					d_upperCost,d_lowerCost;											// upper and lower fitness of individuals in population
+		vector<Solution*> 		d_solutions;     													//		individuals in population
+		vector<struct particle> d_fitnessValues;													//		
+		const SetUpParams& 		d_setup;															//		
+		int 					d_upperCost,d_lowerCost;											//		upper and lower fitness of individuals in population
+																									//		->Peu utile car les valeurs sont triées
+		
+		int partition(int gauche, int droite);														//	OK	QuickSort
+		void QuickSort(int gauche, int droite);														//	OK	QuickSort										
 };
 
 #endif
